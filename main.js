6b0b79f7afb60c6594c3274faebd28f23b8768e8
Alvin Cheng <ascheng21@gmail.com>
2017-06-14 15:40:05 -0700

    The jersey color changes based on color selected. #3

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 var $front = document.querySelector('#front-img')
 var $back = document.querySelector('#back-img')
 var $optionsForm = document.querySelector('#options-form')
+var $primaryColor = document.querySelector('#jersey-color-primary')
+var $secondaryColor = document.querySelector('#jersey-color-secondary')
+var primaryColor = 'white'
+var secondaryColor = 'white'
 
 $optionsForm.addEventListener('submit', function (event) {
   event.preventDefault()
   var $gender = document.querySelector('input[name="gender"]:checked')
   $front.src = 'images/' + $gender.value + '-white-white-front.jpg'
   $back.src = 'images/' + $gender.value + '-white-white-back.jpg'
+})
+
+$primaryColor.addEventListener('change', function (event) {
+  primaryColor = event.target.value
+  $front.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-front.jpg'
+  $back.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-back.jpg'
+})
+
+$secondaryColor.addEventListener('change', function (event) {
+  secondaryColor = event.target.value
+  $front.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-front.jpg'
+  $back.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-back.jpg'
 })
