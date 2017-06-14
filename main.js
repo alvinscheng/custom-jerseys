c034fe224bfac3a3ffba5f30be71c1347b1d33e0
Alvin Cheng <ascheng21@gmail.com>
2017-06-14 16:08:26 -0700

    Minor bug fix. #3

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var $front = document.querySelector('#front-img')
 var $back = document.querySelector('#back-img')
-var $optionsForm = document.querySelector('#options-form')
+var $genderForm = document.querySelector('#gender')
 var $primaryColor = document.querySelector('#jersey-color-primary')
 var $secondaryColor = document.querySelector('#jersey-color-secondary')
 var primaryColor = 'white'
 var secondaryColor = 'white'
 var gender = 'male'
 
-$optionsForm.addEventListener('change', function (event) {
+$genderForm.addEventListener('change', function (event) {
   gender = event.target.value
   $front.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-front.jpg'
   $back.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-back.jpg'
