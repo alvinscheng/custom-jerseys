99712e931656879b30987016c957c04078931824
Alvin Cheng <ascheng21@gmail.com>
2017-06-14 15:49:19 -0700

    Removed the submit button. #3

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ var $primaryColor = document.querySelector('#jersey-color-primary')
 var $secondaryColor = document.querySelector('#jersey-color-secondary')
 var primaryColor = 'white'
 var secondaryColor = 'white'
+var gender = 'male'
 
-$optionsForm.addEventListener('submit', function (event) {
-  event.preventDefault()
-  var $gender = document.querySelector('input[name="gender"]:checked')
-  $front.src = 'images/' + $gender.value + '-white-white-front.jpg'
-  $back.src = 'images/' + $gender.value + '-white-white-back.jpg'
+$optionsForm.addEventListener('change', function (event) {
+  gender = event.target.value
+  $front.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-front.jpg'
+  $back.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-back.jpg'
 })
 
 $primaryColor.addEventListener('change', function (event) {
   primaryColor = event.target.value
-  $front.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-front.jpg'
-  $back.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-back.jpg'
+  $front.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-front.jpg'
+  $back.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-back.jpg'
 })
 
 $secondaryColor.addEventListener('change', function (event) {
   secondaryColor = event.target.value
-  $front.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-front.jpg'
-  $back.src = 'images/male-' + primaryColor + '-' + secondaryColor + '-back.jpg'
+  $front.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-front.jpg'
+  $back.src = 'images/' + gender + '-' + primaryColor + '-' + secondaryColor + '-back.jpg'
 })
