0505b18c803f9e170be2155128af2c9681013f1d
Alvin Cheng <ascheng21@gmail.com>
2017-06-15 14:45:31 -0700

    The jersey number updates when submitted. #9

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var jerseyConfig = {
 
 var $front = document.querySelector('#front-img')
 var $back = document.querySelector('#back-img')
+var $jerseyNumber = document.querySelector('#jersey-number')
+var $nameAndNumber = document.querySelector('#name-number')
 var $genderForm = document.querySelector('#gender')
 var $primaryColor = document.querySelector('#jersey-color-primary')
 var $secondaryColor = document.querySelector('#jersey-color-secondary')
@@ -15,6 +17,7 @@ var $secondaryColor = document.querySelector('#jersey-color-secondary')
 function changeJersey(config) {
   $front.src = 'images/' + config.gender + '-' + config.primaryColor + '-' + config.secondaryColor + '-front.jpg'
   $back.src = 'images/' + config.gender + '-' + config.primaryColor + '-' + config.secondaryColor + '-back.jpg'
+  $jerseyNumber.textContent = config.number
 }
 
 $genderForm.addEventListener('change', function (event) {
@@ -29,5 +32,12 @@ $primaryColor.addEventListener('change', function (event) {
 
 $secondaryColor.addEventListener('change', function (event) {
   jerseyConfig.secondaryColor = event.target.value
+  changeJersey(jerseyConfig)
+})
+
+$nameAndNumber.addEventListener('submit', function (event) {
+  event.preventDefault()
+  var $number = document.querySelector('#number')
+  jerseyConfig.number = $number.value
   changeJersey(jerseyConfig)
 })
